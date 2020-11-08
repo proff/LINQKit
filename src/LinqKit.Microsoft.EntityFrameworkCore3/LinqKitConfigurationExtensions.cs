cc91f7c4c505a2e25abb9acc4c03856c2bbf0914
proff <proff@proffmail.ru>
2020-11-09 01:10:29 +0300

    integration to EF Core configuration

---
+++ src/LinqKit.Microsoft.EntityFrameworkCore3/LinqKitConfigurationExtensions.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Linq.Expressions;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Query;
+using Microsoft.EntityFrameworkCore.Query.Internal;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace LinqKit.Microsoft.EntityFrameworkCore3
+{
+    public static class LinqKitConfigurationExtensions
+    {
+        public static DbContextOptionsBuilder UseLinqKit(this DbContextOptionsBuilder optionsBuilder)
+        {
+            Type type = null;
+            if (optionsBuilder.Options.FindExtension<CoreOptionsExtension>()?.ReplacedServices
+#if EFCORE5
+                ?.TryGetValue((typeof(IQueryTranslationPreprocessorFactory), null), out type) == true)
+#else
+                ?.TryGetValue(typeof(IQueryTranslationPreprocessorFactory), out type) == true)
+#endif                
+            {
+                var extension = optionsBuilder.Options.FindExtension<LinqKitOptionsExtension>() ??
+                                new LinqKitOptionsExtension();
+                extension.PreviousQueryTranslationPreprocessorFactory = type;
+                ((IDbContextOptionsBuilderInfrastructure) optionsBuilder).AddOrUpdateExtension(extension);
+            }
+
+            optionsBuilder
+                .ReplaceService<IQueryTranslationPreprocessorFactory, LinqKitQueryTranslationPreprocessorFactory>();
+            return optionsBuilder;
+        }
+
+        private class LinqKitOptionsExtension : IDbContextOptionsExtension
+        {
+            private ExtensionInfo _info;
+            public Type PreviousQueryTranslationPreprocessorFactory { get; set; }
+
+            public void ApplyServices(IServiceCollection services)
+            {
+            }
+
+            public void Validate(IDbContextOptions options)
+            {
+            }
+
+            public DbContextOptionsExtensionInfo Info => _info = _info ?? new ExtensionInfo(this);
+
+            private class ExtensionInfo : DbContextOptionsExtensionInfo
+            {
+                public ExtensionInfo(LinqKitOptionsExtension extension) : base(extension)
+                {
+                }
+
+                private new LinqKitOptionsExtension Extension => (LinqKitOptionsExtension) base.Extension;
+
+                public override bool IsDatabaseProvider => false;
+                public override string LogFragment => string.Empty;
+
+                public override long GetServiceProviderHashCode()
+                {
+                    return Extension.PreviousQueryTranslationPreprocessorFactory?.GetHashCode() ?? 0L;
+                }
+
+                public override void PopulateDebugInfo(IDictionary<string, string> debugInfo)
+                {
+                    if (Extension.PreviousQueryTranslationPreprocessorFactory != null)
+                        debugInfo["LinqKit:" + nameof(PreviousQueryTranslationPreprocessorFactory)] = Extension
+                            .PreviousQueryTranslationPreprocessorFactory.GetHashCode()
+                            .ToString(CultureInfo.InvariantCulture);
+                }
+            }
+        }
+
+        private class LinqKitQueryTranslationPreprocessor : QueryTranslationPreprocessor
+        {
+            private readonly QueryTranslationPreprocessor _next;
+
+            public LinqKitQueryTranslationPreprocessor(QueryTranslationPreprocessorDependencies dependencies,
+                QueryCompilationContext queryCompilationContext, QueryTranslationPreprocessor next) : base(dependencies,
+                queryCompilationContext)
+            {
+                _next = next;
+            }
+
+            public override Expression Process(Expression query)
+            {
+                // base method will be called in _next
+                return _next.Process(query.Expand());
+            }
+        }
+
+        private class LinqKitQueryTranslationPreprocessorFactory : IQueryTranslationPreprocessorFactory
+        {
+            private readonly QueryTranslationPreprocessorDependencies _dependencies;
+
+            private readonly ConcurrentDictionary<Type, IQueryTranslationPreprocessorFactory> _factories =
+                new ConcurrentDictionary<Type, IQueryTranslationPreprocessorFactory>();
+
+            private readonly IServiceProvider _serviceProvider;
+
+            public LinqKitQueryTranslationPreprocessorFactory(QueryTranslationPreprocessorDependencies dependencies,
+                RelationalQueryTranslationPreprocessorDependencies relationalDependencies,
+                IServiceProvider serviceProvider)
+            {
+                _dependencies = dependencies;
+                _serviceProvider = serviceProvider;
+            }
+
+
+            public QueryTranslationPreprocessor Create(QueryCompilationContext queryCompilationContext)
+            {
+                var options = queryCompilationContext.ContextOptions.FindExtension<LinqKitOptionsExtension>();
+                var type = options?.PreviousQueryTranslationPreprocessorFactory ??
+                           typeof(RelationalQueryTranslationPreprocessorFactory);
+                var factory = _factories.GetOrAdd(type, t => GetService(t));
+                return new LinqKitQueryTranslationPreprocessor(_dependencies, queryCompilationContext,
+                    factory.Create(queryCompilationContext));
+            }
+
+            private IQueryTranslationPreprocessorFactory GetService(Type t)
+            {
+                var constructorInfo = t.GetConstructors().OrderByDescending(a => a.GetParameters().Length).First();
+                var factory = constructorInfo.Invoke(constructorInfo.GetParameters()
+                    .Select(a => _serviceProvider.GetRequiredService(a.ParameterType)).ToArray());
+                return (IQueryTranslationPreprocessorFactory) factory;
+            }
+        }
+    }
+}
---
+++ tests/LinqKit.Microsoft.EntityFrameworkCore3.Tests/ConfigurationIntegrationTests.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using LinqKit.EntityFramework.Tests;
+using LinqKit.Microsoft.EntityFrameworkCore3;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Query;
+using Xunit;
+
+namespace LinqKit.Microsoft.EntityFrameworkCore.Tests
+{
+    public class ConfigurationIntegrationTests : IDisposable
+    {
+        private TestContext _db;
+
+        public void Dispose()
+        {
+            _db.Database.EnsureDeleted();
+            _db.Dispose();
+            _db = null;
+        }
+
+        public void Init()
+        {
+            _db.Database.EnsureCreated();
+
+            _db.Entities.RemoveRange(_db.Entities.ToList());
+            _db.Entities.AddRange(new Entity {Value = 123}, new Entity {Value = 67}, new Entity {Value = 3});
+            _db.SaveChanges();
+        }
+
+        [Fact]
+        public async Task WithDefaultQueryTranslationPreprocessorFactory()
+        {
+            var builder = new DbContextOptionsBuilder();
+            builder.UseSqlite($"Filename=LinqKit.{Guid.NewGuid()}.db");
+            builder.UseLinqKit();
+            _db = new TestContext(builder.Options);
+
+            Init();
+            var eParam = Expression.Parameter(typeof(Entity), "e");
+            var eProp = Expression.PropertyOrField(eParam, "Value");
+
+            var conditions =
+                (from item in new List<int> {10, 20, 30, 80}
+                    select Expression.LessThan(eProp, Expression.Constant(item))).Aggregate(Expression.OrElse);
+
+            var combined = Expression.Lambda<Func<Entity, bool>>(conditions, eParam);
+
+            var q = from e in _db.Entities where combined.Invoke(e) select new {e.Value};
+
+            var res = await q.ToListAsync().ConfigureAwait(false);
+
+            Assert.Equal(2, res.Count);
+            Assert.Equal(67, res.First().Value);
+        }
+
+        [Fact]
+        public async Task WithCustomQueryTranslationPreprocessorFactory()
+        {
+            try
+            {
+                var builder = new DbContextOptionsBuilder();
+                builder.UseSqlite($"Filename=LinqKit.{Guid.NewGuid()}.db");
+                builder.ReplaceService<IQueryTranslationPreprocessorFactory, TestQueryTranslationPreprocessorFactory>();
+                builder.UseLinqKit();
+                _db = new TestContext(builder.Options);
+
+                Init();
+                var eParam = Expression.Parameter(typeof(Entity), "e");
+                var eProp = Expression.PropertyOrField(eParam, "Value");
+
+                var conditions =
+                    (from item in new List<int> {10, 20, 30, 80}
+                        select Expression.LessThan(eProp, Expression.Constant(item))).Aggregate(Expression.OrElse);
+
+                var combined = Expression.Lambda<Func<Entity, bool>>(conditions, eParam);
+
+                var q = from e in _db.Entities where combined.Invoke(e) select new {e.Value};
+
+                var res = await q.ToListAsync().ConfigureAwait(false);
+
+                Assert.Equal(2, res.Count);
+                Assert.Equal(67, res.First().Value);
+                Assert.True(TestQueryTranslationPreprocessor.Executed);
+            }
+            finally
+            {
+                TestQueryTranslationPreprocessor.Executed = false;
+            }
+        }
+
+        private class TestQueryTranslationPreprocessorFactory : IQueryTranslationPreprocessorFactory
+        {
+            private readonly QueryTranslationPreprocessorDependencies _dependencies;
+
+            public TestQueryTranslationPreprocessorFactory(QueryTranslationPreprocessorDependencies dependencies,
+                IServiceProvider serviceProvider)
+            {
+                _dependencies = dependencies;
+            }
+
+            public QueryTranslationPreprocessor Create(QueryCompilationContext queryCompilationContext)
+            {
+                return new TestQueryTranslationPreprocessor(_dependencies, queryCompilationContext);
+            }
+        }
+    }
+
+    public class TestQueryTranslationPreprocessor : QueryTranslationPreprocessor
+    {
+        public TestQueryTranslationPreprocessor(QueryTranslationPreprocessorDependencies dependencies,
+            QueryCompilationContext queryCompilationContext) : base(dependencies, queryCompilationContext)
+        {
+        }
+
+        public static bool Executed { get; set; }
+
+        public override Expression Process(Expression query)
+        {
+            Executed = true;
+            return base.Process(query);
+        }
+    }
+}
